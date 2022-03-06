a70a20a185f86abc12a907613dc7aa836de6d06b
Александр Ромахин <asromahin@yandex.ru>
2022-03-06 14:29:30 +0700

    from yolov5 detection

--- converters/yolov5.py
+++ converters/yolov5.py
@@ -2,11 +2,12 @@ from tqdm import tqdm
 import os
 from shutil import copyfile
 import yaml
+import cv2
 
 from model import DetectionInfo, ImageInfo, ImagesInfo
 
 
-def to_yolov5(images_info: ImagesInfo, save_path: str):
+def to_yolov5(images_info: ImagesInfo, save_path: str, name_to_code: dict = None):
     train_path = os.path.join(save_path, 'train')
 
     train_path_images = os.path.join(train_path, 'images')
@@ -24,7 +25,8 @@ def to_yolov5(images_info: ImagesInfo, save_path: str):
     os.makedirs(val_path_images, exist_ok=True)
     os.makedirs(val_path_labels, exist_ok=True)
 
-    cat_code = dict()
+    if name_to_code is None:
+        name_to_code = dict()
 
     for image in tqdm(images_info.images):
         image: ImageInfo
@@ -46,9 +48,9 @@ def to_yolov5(images_info: ImagesInfo, save_path: str):
             w = str(detection.w)
             h = str(detection.h)
             name = detection.name
-            if name not in cat_code.keys():
-                cat_code[name] = len(cat_code.keys())
-            name_code = str(cat_code[name])
+            if name not in name_to_code.keys():
+                name_to_code[name] = len(name_to_code.keys())
+            name_code = str(name_to_code[name])
             res_str = '\t'.join([name_code, center_x, center_y, w, h])
             lines.append(res_str)
         res_data = '\n'.join(lines)
@@ -59,9 +61,58 @@ def to_yolov5(images_info: ImagesInfo, save_path: str):
         'path': save_path,
         'train': train_path,
         'val': val_path,
-        'nc': len(cat_code.keys()),
-        'names': list(cat_code.keys()),
+        'nc': len(name_to_code.keys()),
+        'names': list(name_to_code.keys()),
     }
-    # print(dict_file)
+
     with open(dataset_path, 'w') as f:
         yaml.dump(dict_file, f)
+
+
+def from_yolov5_detections(images_folder, txt_folder, code_to_name, split='test'):
+    image_names = os.listdir(images_folder)
+    images = []
+    for image_name in tqdm(image_names):
+        filename = image_name.split('.')[0]
+        txt_name = filename + '.txt'
+
+        txt_path = os.path.join(txt_folder, txt_name)
+        im_path = os.path.join(images_folder, image_name)
+
+        im = cv2.imread(im_path)
+        im_width = im.shape[1]
+        im_height = im.shape[0]
+        detections = []
+        with open(txt_path, 'r') as f:
+            data = f.read()
+            lines = data.split('\n')
+            for line in lines:
+                name_code, center_x, center_y, w, h, conf = line.split('\t')
+                name_code = int(name_code)
+                center_x = float(center_x)
+                center_y = float(center_y)
+                w = float(w)
+                h = float(h)
+                conf = float(conf)
+                name = code_to_name[name_code]
+                det = DetectionInfo(name=name, x=center_x, y=center_y, w=w, h=h)
+                detections.append(det)
+        images.append(
+            ImageInfo(
+                name=None,
+                filename=image_name,
+                path=im_path,
+                width=im_width,
+                height=im_height,
+                detections=detections,
+                split=split,
+            )
+        )
+        images_info = ImagesInfo(name='yolov5_detections', images=images)
+        return images_info
+
+
+if __name__ == '__main__':
+    from_yolov5_detections(
+
+    )
